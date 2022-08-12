01602f398bd48dbc4a73c756a60cb1c1a2cc6ff8
Leiner Barrios Medina <leiner.barrios@inglesparatodos.edu.co>
2022-08-11 22:14:01 -0500

    Prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
