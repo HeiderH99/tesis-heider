bd3105440ae8f260b692898e949bac865f1f4cd0
Leiner Barrios Medina <leiner.barrios@inglesparatodos.edu.co>
2022-08-14 13:43:09 -0500

    Init information view

---
+++ src/app/tesis/components/cancer-section/cancer-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancer-section',
+  templateUrl: './cancer-section.component.html',
+  styleUrls: ['./cancer-section.component.css']
+})
+export class CancerSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/tesis/components/docs-section/docs-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-docs-section',
+  templateUrl: './docs-section.component.html',
+  styleUrls: ['./docs-section.component.css']
+})
+export class DocsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
